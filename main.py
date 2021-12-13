36b7bd448f118b4028b90a7f49d5c45b16b457f0
Marrek Nožka <nozka@spseol.cz>
2021-12-13 09:33:01 +0100

    změna rozmístění a přídání funkcí

--- main.py
+++ main.py
@@ -2,40 +2,62 @@
 
 from os.path import basename, splitext
 import tkinter as tk
-from tkinter import Canvas, Scale, HORIZONTAL
+from tkinter import Canvas, Scale, HORIZONTAL, LEFT, Frame, Entry, S, END, StringVar
 
 # from tkinter import ttk
 
 
 class Application(tk.Tk):
     name = basename(splitext(basename(__file__.capitalize()))[0])
-    name = "ColoMishMash"
+    name = "ColorMishMash"
 
     def __init__(self):
         super().__init__(className=self.name)
         self.title(self.name)
 
         self.bind("<Escape>", self.quit)  # klávesa Esc spustí funkci quit()
-
-        self.lblR = tk.Label(self, text="R")
-        self.lblR.pack()
+        
+        self.frameR = Frame(self)
+        self.frameR.pack()
+        self.frameG = Frame(self)
+        self.frameG.pack()
+        self.frameB = Frame(self)
+        self.frameB.pack()
+        ### R
+        self.lblR = tk.Label(self.frameR, text="R:")
+        self.lblR.pack(side=LEFT, anchor=S)
         self.scaleR = Scale(
-            self, from_=0, to=255, orient=HORIZONTAL, length=256, command=self.change
+            self.frameR, from_=0, to=255, orient=HORIZONTAL, length=256, command=self.change
         )
-        self.scaleR.pack()
-
-        self.lblG = tk.Label(self, text="G")
-        self.lblG.pack()
-        self.scaleG = Scale(self, from_=0, to=255, orient=HORIZONTAL, length=256)
-        self.scaleG.pack()
-
-        self.lblB = tk.Label(self, text="B")
-        self.lblB.pack()
-        self.scaleB = Scale(self, from_=0, to=255, orient=HORIZONTAL, length=256)
-        self.scaleB.pack()
+        self.scaleR.pack(side=LEFT, anchor=S)
+        self.varR = StringVar()
+        self.entryR = Entry(self.frameR, width=4, textvariable=self.varR)
+        self.entryR.pack(side=LEFT, anchor=S)
+        ### G
+        self.lblG = tk.Label(self.frameG, text="G:")
+        self.lblG.pack(side=LEFT, anchor=S)
+        self.scaleG = Scale(
+            self.frameG,from_=0, to=255, orient=HORIZONTAL, length=256, command=self.change,
+        )
+        self.scaleG.pack(side=LEFT, anchor=S)
+        self.varG = StringVar()
+        self.entryG = Entry(self.frameG, width=4, textvariable=self.varG)
+        self.entryG.pack(side=LEFT, anchor=S)
+        ### B
+        self.lblB = tk.Label(self.frameB, text="B:")
+        self.lblB.pack(side=LEFT, anchor=S)
+        self.scaleB = Scale(
+            self.frameB, from_=0, to=255, orient=HORIZONTAL, length=256, command=self.change
+        )
+        self.scaleB.pack(side=LEFT, anchor=S)
+        self.varB = StringVar()
+        self.entryB = Entry(self.frameB, width=4, textvariable=self.varB)
+        self.entryB.pack(side=LEFT, anchor=S)
 
         self.canvasMain = Canvas(self, width=256, height=100, background="#000000")
         self.canvasMain.pack()
+        self.entryMain = Entry(self,)
+        self.entryMain.pack(side=LEFT)
 
         self.btnQuit = tk.Button(self, text="Quit", command=self.quit)
         self.btnQuit.pack()
@@ -47,7 +69,14 @@ class Application(tk.Tk):
         r = self.scaleR.get()
         g = self.scaleG.get()
         b = self.scaleB.get()
-        self.canvasMain.config(background=f"#{r:02x}{g:02x}{b:02x}")
+        colorcode = f"#{r:02x}{g:02x}{b:02x}"
+        self.canvasMain.config(background=colorcode)
+        self.entryMain.delete(0, END)
+        self.entryMain.insert(0, colorcode)
+
+        self.varR.set(r)
+        self.varG.set(g)
+        self.varB.set(b)
 
     def quit(self, event=None):
         super().quit()
